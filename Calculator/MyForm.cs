543b8cd03b00e404fde9511ca222a537b74f9e7e
mmmolin78 <mmmolin78@gmail.com>
2017-12-06 00:04:32 +0100

    set up tablelayoutpanel

---
+++ Calculator/MyForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Calculator
+{
+    class MyForm : Form
+    {
+        public MyForm()
+        {
+            TableLayoutPanel table = new TableLayoutPanel
+            {
+                ColumnCount = 4,
+                RowCount = 8,
+                BackColor = Color.Beige,
+                Dock = DockStyle.Fill
+            };
+            Controls.Add(table);
+            
+        }
+    }
+}
