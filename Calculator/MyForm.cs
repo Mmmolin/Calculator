c632391e710109feabb31631d7995233574ace0e
mmmolin78 <mmmolin78@gmail.com>
2017-12-06 15:29:24 +0100

    added textBox

--- Calculator/MyForm.cs
+++ Calculator/MyForm.cs
@@ -15,12 +15,25 @@ namespace Calculator
             TableLayoutPanel table = new TableLayoutPanel
             {
                 ColumnCount = 4,
-                RowCount = 8,
+                RowCount = 6,
                 BackColor = Color.Beige,
                 Dock = DockStyle.Fill
             };
             Controls.Add(table);
-            
+            table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25));
+            table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25));
+            table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25));
+            table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25));
+
+            TextBox mainIO = new TextBox
+            {
+                Dock = DockStyle.Fill
+            };
+            table.Controls.Add(mainIO);
+            table.SetColumnSpan(mainIO, 4);
+            table.SetRowSpan(mainIO, 2);           
         }
+
+
     }
 }
