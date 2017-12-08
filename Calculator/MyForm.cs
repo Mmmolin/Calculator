9b20c340d1b09cc6e042a14743cdc380e243ec58
mmmolin78 <mmmolin78@gmail.com>
2017-12-08 08:54:39 +0100

    added button features

--- Calculator/MyForm.cs
+++ Calculator/MyForm.cs
@@ -10,6 +10,8 @@ namespace Calculator
 {
     class MyForm : Form
     {
+        TextBox mainIO;
+        List<object> NumbersAndOperators = new List<object> { };
         public MyForm()
         {
             MinimumSize = new Size(330, 360);
@@ -34,7 +36,7 @@ namespace Calculator
             table.RowStyles.Add(new RowStyle(SizeType.Percent, 20));
             table.RowStyles.Add(new RowStyle(SizeType.Percent, 20));
 
-            TextBox mainIO = new TextBox
+            mainIO = new TextBox
             {
                 Dock = DockStyle.Fill,
                 Font = new Font("serif", 24),
@@ -43,31 +45,13 @@ namespace Calculator
             table.Controls.Add(mainIO);
             table.SetColumnSpan(mainIO, 4);
 
-            table.Controls.Add(ButtonMaker("CE"));
-            table.Controls.Add(ButtonMaker("C"));
-            table.Controls.Add(ButtonMaker("<="));
-            table.Controls.Add(ButtonMaker("/"));
-
-            table.Controls.Add(ButtonMaker("7"));
-            table.Controls.Add(ButtonMaker("8"));
-            table.Controls.Add(ButtonMaker("9"));
-            table.Controls.Add(ButtonMaker("X"));
-
-            table.Controls.Add(ButtonMaker("4"));
-            table.Controls.Add(ButtonMaker("5"));
-            table.Controls.Add(ButtonMaker("6"));
-            table.Controls.Add(ButtonMaker("-"));
-
-            table.Controls.Add(ButtonMaker("1"));
-            table.Controls.Add(ButtonMaker("2"));
-            table.Controls.Add(ButtonMaker("3"));
-            table.Controls.Add(ButtonMaker("+"));
-
-            table.Controls.Add(ButtonMaker("∓"));
-            table.Controls.Add(ButtonMaker("0"));
-            table.Controls.Add(ButtonMaker(","));
-            table.Controls.Add(ButtonMaker("="));
-
+            string[] buttonText = { "CE", "C", "<=", "/", "7", "8", "9", "X", "4", "5", "6", "-", "1", "2", "3", "+", "∓", "0", ",", "=" };
+            foreach (string text in buttonText)
+            {
+                table.Controls.Add(ButtonMaker(text));
+                table.Controls[table.Controls.Count - 1].Click += ClickedKeyEventHandler;
+                //GetControl(text).Click += ClickedKeyEventHandler;
+            }
         }
         public Button ButtonMaker(string text)
         {
@@ -77,6 +61,46 @@ namespace Calculator
                 Dock = DockStyle.Fill
             };
             return button;
+        }
+        public Control GetControl(string text)
+        {
+            Control returnThis = null;
+            foreach (Control control in Controls)
+            {
+                if (control.Text == text)
+                {
+                    returnThis = control;
+                }
+            }
+            return returnThis;
+        }
+        public void ClickedKeyEventHandler(object sender, EventArgs e)
+        {
+            Button recieved = (Button)sender;
+            string keyPressed = recieved.Text.ToString();
+            switch(keyPressed)
+            {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                case "0":
+                    mainIO.Text += keyPressed;
+                    break;
+                case "C":
+                    mainIO.Clear();
+                    break;
+                case "+":
+                case "-":
+                case "/":
+                case "X":                    
+                    break;
+            }
         }
     }
 }
